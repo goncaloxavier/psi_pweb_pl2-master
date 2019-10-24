a045460b55464b6a337c88df16d930c54d2ff0a3
2190788 <2190788@my.ipleiria.pt>
2019-10-24 12:27:25 +0100

    completed ficha 3 ex 3

--- Ficha 3/Ex 3/F3_ex3.script.js
+++ Ficha 3/Ex 3/F3_ex3.script.js
@@ -1,5 +1,35 @@
 "use strict";
 
-function escondeMostra() {
+var imagem = null;
 
+function escondeMostra() {
+    console.log("esconde/mostra");
+
+    var visibilidade = getComputedStyle(imagem).getPropertyValue("visibility");
+
+    console.log("Estilo atual: " + visibilidade);
+
+    if(visibilidade === "hidden"){
+        imagem.style.visibility = "visible";
+    }
+    else{
+        imagem.style.visibility = "hidden";
+    }
 }
+
+function esconde() {
+    console.log("esconde");
+    imagem.style.display = "none";
+}
+
+function mostra() {
+    console.log("mostra");
+    imagem.style.display = "inline-block";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    imagem = document.getElementsByTagName("img")[0];
+
+    var botao = document.getElementById("botao");
+    botao.addEventListener("mouseover", mostra);
+})
